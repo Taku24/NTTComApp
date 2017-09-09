1463289fcf08a19a3697f745ad74d74a1ddff59f
Taku24 <taku178.24@hotmail.co.jp>
2017-09-09 13:25:13 +0900

    Add ViewController

---
+++ NTTComApp/InitialViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InitialViewController.swift
+//  NTTComApp
+//
+//  Created by TAKU on 2017/09/09.
+//  Copyright © 2017年 NttCmmApp. All rights reserved.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
--- NTTComApp/SyncViewController.swift
+++ NTTComApp/SyncViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SyncViewController.swift
 //  NTTComApp
 //
 //  Created by TAKU on 2017/09/09.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SyncViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
